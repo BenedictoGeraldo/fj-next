51f84d2f6eda4441227bf5d9f799a4d7539dbf2c
BenedictoGeraldo <geraldodawa@gmail.com>
2025-12-23 03:47:13 +0700

    back to day 1: SSR vs CSR fix

--- app/learning/day-1-fundamentals/02-server-client/page.tsx
+++ app/learning/day-1-fundamentals/02-server-client/page.tsx
@@ -1,50 +1,48 @@
-// import ClientRouter from "./ClientRouter";
+import ClientRouter from "./ClientRouter";
 
-// export default function ServerClient() {
-//   return (
-//     <div className="min-h-screen bg-white text-black">
-//       <h1>Mempelajari beda SSR dan CSR</h1>
-//       <p>
-//         SSR itu tidak bisa menggunakan hooks. biasanya digunakan untuk
-//         menampilkan data statis, bukan yang dinamis atau butuh interakhis
-//       </p>
-//       <p>
-//         CSR bisa menggunakan hooks. biasa digunakan untuk menampilkan data
-//         dinamis yang butuh perubahan cepat
-//       </p>
-
-//       <section>
-//         <h2>Server Component</h2>
-//         <ServerDataDisplay />
-//       </section>
-
-//       <section>
-//         <h2>Client Component</h2>
-//         <ClientRouter />
-//       </section>
-//     </div>
-//   );
-// }
-
-// async function ServerDataDisplay() {
-//   return (
-//     <div>
-//       <p>Komponen ini di render di Server</p>
-//     </div>
-//   );
-// }
-
-"use client";
-
-import { useState } from "react";
-
-export default function ServerHuk() {
-  const [count, setCount] = useState(1);
+export default function ServerClient() {
   return (
-    <div>
-      <h1> Tes SSR tanpa use Client with hooks</h1>
-      <p>count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Tambah +</button>
+    <div className="min-h-screen bg-white text-black">
+      <h1>Mempelajari beda SSR dan CSR</h1>
+      <p>
+        SSR itu tidak bisa menggunakan hooks. biasanya digunakan untuk
+        menampilkan data statis, bukan yang dinamis atau butuh interakhis
+      </p>
+      <p>
+        CSR bisa menggunakan hooks. biasa digunakan untuk menampilkan data
+        dinamis yang butuh perubahan cepat
+      </p>
+
+      <section>
+        <h2>Server Component</h2>
+        <ServerDataDisplay />
+      </section>
+
+      <section>
+        <h2>Client Component</h2>
+        <ClientRouter />
+      </section>
     </div>
   );
 }
+
+async function ServerDataDisplay() {
+  return (
+    <div>
+      <p>Komponen ini di render di Server</p>
+    </div>
+  );
+}
+
+// import { useState } from "react";
+
+// export default function ServerHuk() {
+//   const [count, setCount] = useState(1);
+//   return (
+//     <div>
+//       <h1> Tes SSR tanpa use Client with hooks</h1>
+//       <p>count: {count}</p>
+//       <button onClick={() => setCount(count + 1)}>Tambah +</button>
+//     </div>
+//   );
+// }
