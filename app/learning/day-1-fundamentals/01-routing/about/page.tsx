11ba1e0af2599611161ee6ddddf87dda396ffbfd
BenedictoGeraldo <geraldodawa@gmail.com>
2025-12-23 03:16:31 +0700

    update day 1: basic routing

---
+++ app/learning/day-1-fundamentals/01-routing/about/page.tsx
@@ -0,0 +1,12 @@
+export default function aboutPage() {
+  return (
+    <div className="min-h-screen bg-white text-black">
+      <h1>
+        Ini adalah halaman about. gunakan ini untuk mempelajri dasar routing
+      </h1>
+      <a href="/learning/day-1-fundamentals/01-routing">
+        Kembali ke halaman tadi
+      </a>
+    </div>
+  );
+}
---
+++ app/learning/day-1-fundamentals/01-routing/page.tsx
@@ -0,0 +1,16 @@
+export default function HomePage() {
+  return (
+    <div className="min-h-screen bg-white text-black">
+      <h1>
+        {" "}
+        This is home page, gunakan file ini untuk mempelajari bagaimana
+        penggunaan homepage
+      </h1>
+      <div>
+        <a href="/learning/day-1-fundamentals/01-routing/about">
+          Pergi ke about page
+        </a>
+      </div>
+    </div>
+  );
+}
