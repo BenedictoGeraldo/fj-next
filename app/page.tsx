2fd28776cd79ea77397d5fd2cc6682de8ee71467
BenedictoGeraldo <geraldodawa@gmail.com>
2026-04-24 00:47:37 +0700

    learning fething data

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,11 @@ import { redirect } from "next/navigation";
 
 export default function main() {
   // redirect("/learning/day-1-fundamentals/01-routing");
-
   // redirect("/learning/day-1-fundamentals/02-server-client");
   // redirect("/learning/day-1-fundamentals/03-layouts");
   // redirect("/learning/day-1-fundamentals/04-dynamic-routes");
   // redirect("/learning/day-2-data-api/01-fetch-server");
-  // redirect("/learning/day-2-data-api/02-fetch-client");
+  redirect("/learning/day-2-data-api/02-fetch-client");
   // redirect("/learning/day-2-data-api/03-api-routes");
   // redirect("/learning/day-2-data-api/04-post-data");
   // redirect("/learning/day-2-data-api/05-error-loading");
@@ -16,5 +15,5 @@ export default function main() {
   // redirect("/learning/day-3-libraries/axios/axios-put");
   // redirect("/learning/day-3-libraries/axios/axios-delete");
   // redirect("/learning/day-3-libraries/zustand");
-  redirect("/learning/day-3-libraries/zod");
+  // redirect("/learning/day-3-libraries/zod");
 }
